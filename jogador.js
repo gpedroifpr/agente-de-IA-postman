b0fae20a2c6a7cef74002fa804126c166980ce58
gpedroifpr <gpedroifpr@gmail.com>
2026-09-23 07:59:07 -0300

    Delete jogador.js

--- jogador.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const JogadorSchema = new mongoose.Schema({
-    nome: { type: String, required: true, unique: true },
-    xp: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('Jogador', JogadorSchema);
